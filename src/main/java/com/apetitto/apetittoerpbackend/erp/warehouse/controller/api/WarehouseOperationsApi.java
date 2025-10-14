02b0bbfbbb5c137749dbb7241befacb17ac5e11a
Asilbek <asilbekrakhmonov1@gmail.com>
2025-10-14 19:35:55 +0500

    Please worck with H2 basegit add .!

--- src/main/java/com/apetitto/apetittoerpbackend/erp/warehouse/controller/api/WarehouseOperationsApi.java
+++ src/main/java/com/apetitto/apetittoerpbackend/erp/warehouse/controller/api/WarehouseOperationsApi.java
@@ -4,7 +4,6 @@ import com.apetitto.apetittoerpbackend.erp.warehouse.dto.StockItemDto;
 import com.apetitto.apetittoerpbackend.erp.warehouse.dto.StockMovementRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
